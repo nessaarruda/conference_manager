65287ede985cba47eb58e922ad61ef12bb72e6db
Grayson Cunningham <gecun289@gmail.com>
2020-12-01 18:13:45 -0700

    Create initial tests for conference view

---
+++ spec/features/conferences/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  describe 'conferences index page' do
+    it "can see the name of each parent record in the system" do
+      conference_1 = Conference.create(name: "World Ruby Conference",
+                                      organization: "Ruby Association",
+                                      size: 2000,
+                                      start_date: "2021-01-19",
+                                      end_date: "2021-01-21")
+      conference_2 = Conference.create(name: "Creating Change",
+                                      organization: "National LGBTQ Task Force",
+                                      size: 6000,
+                                      start_date: "2020-01-21",
+                                      end_date: "2020-01-24")
+
+      visit "/songs"
+
+      expect(page).to have_content("Conference List")
+      expect(page).to have_content(conference_1.name)
+      expect(page).to have_content("#{conference_1.start_date} - #{conference_1.end_date}")
+      expect(page).to have_content(conference_2.name)
+      expect(page).to have_content("#{conference_2.start_date} - #{conference_2.end_date}")
+    end
+  end
+end
