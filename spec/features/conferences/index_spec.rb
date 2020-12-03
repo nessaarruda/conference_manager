b871e74900c6c2d665eb9a6b2b1904b4346bd6dd
Grayson Cunningham <gecun289@gmail.com>
2020-12-02 17:02:43 -0700

    Add tests for creating a new Conference

--- spec/features/conferences/index_spec.rb
+++ spec/features/conferences/index_spec.rb
@@ -19,7 +19,20 @@ describe 'as a visitor' do
       expect(page).to have_content("Conference List")
       expect(page).to have_content(conference_1.name)
       expect(page).to have_content(conference_2.name)
+    end
+
+    it "has a link to create a new Conference record" do
+      visit "/conferences"
+
       expect(page).to have_link("New Conference", href: '/conferences/new')
+    end
+
+    it "navigates to the /conferences page from the link to create a new conference" do
+      visit "/conferences"
+
+      click_link("New Conference")
+
+      expect(page).to have_current_path('/conferences/new')
     end
   end
 end
---
+++ spec/features/conferences/new_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  describe 'new conference record page' do
+    it 'displays a form for creating a new conference record' do
+      visit '/conferences/new'
+
+      expect(page).to have_content("New Conference")
+      expect(page).to have_selector('form')
+      expect(page).to have_text('Conference name:')
+      expect(page).to have_field('confname', type: 'text')
+      expect(page).to have_text('Organization:')
+      expect(page).to have_field('org', type: 'text')
+      expect(page).to have_text('Expected attendees:')
+      expect(page).to have_field('size', type: 'bigint')
+      expect(page).to have_text('Dates:')
+      expect(page).to have_field('dates', type: 'date')
+      expect(page).to have_text('to')
+    end
+
+    it 'has a button to Create Conference' do
+      visit '/conferences/new'
+
+      expect(page).to have_button('Create Conference', type: 'submit')
+    end
+
+    it 'creates a new conference and redirects to the conferences index' do
+      visit '/conferences/new'
+
+      fill_in('confname', with: 'World Ruby Conference')
+      fill_in('org', with: 'Ruby Association')
+      fill_in('size', with: 2000)
+      click_on('Create Conference')
+
+      expect(page).to have_current_path('/conferences')
+      expect(page).to have_content("World Ruby Conference")
+    end
+  end
+end
