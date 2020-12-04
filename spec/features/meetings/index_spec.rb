84e6ca24a9d59277a73309039364efe4b33c977e
Vanessa Arruda <wanessaarruda@gmail.com>
2020-12-04 15:59:58 -0700

    Add attributes to story 6

--- spec/features/meetings/index_spec.rb
+++ spec/features/meetings/index_spec.rb
@@ -7,13 +7,17 @@ describe 'As a visitor' do
                                       has_projector: true,
                                       capacity: 30
                                     )
-      @meeting_1 = meeting_room_1.meetings.create!(name: "Monday Meeting",
+      meeting_1 = meeting_room_1.meetings.create!(name: "Monday Meeting",
                                   schedule: "Monday, 10am",
                                   number_of_participants: 10,
                                   duration: "1 hour")
     visit '/meetings'
 
     expect(page).to have_content("Meetings")
+    expect(page).to have_content(meeting_1.name)
+    expect(page).to have_content(meeting_1.schedule)
+    expect(page).to have_content(meeting_1.number_of_participants)
+    expect(page).to have_content(meeting_1.duration)
     end
   end
 end
